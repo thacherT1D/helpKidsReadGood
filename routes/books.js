20d3cab7bf7adc3117a23d6ad1718f705091e49f
THACHER <sophie.thacher@gmail.com>
2016-04-02 17:21:19 -0700

    debugging update and delete

--- routes/books.js
+++ routes/books.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 var knex = require('../db/knex');
-function Books() {
+function books() {
   return knex('books');
 }
 
 router.get('/books', function(req, res, next) {
-  books().select().then(function() {
-    res.render('books/', {allBooks: records});
+  books().select().then(function(records) {
+    res.render('books/index',
+    {allBooks: records});
   });
 });
 
@@ -18,7 +19,10 @@ router.get('/books/new', function(req, res, next) {
 
 router.post('/books', function(req, res, next) {
   books().insert({
-    name: req.body.book_name
+    name: req.body.book_name,
+    author: req.body.book_author,
+    genre: req.body.book_genre,
+    rating: req.body.book_rating
   }).then(function () {
     res.redirect('/books');
   });
@@ -28,6 +32,29 @@ router.get('/books/:id', function(req, res, next) {
   books().where({ id: req.params.id}).first().then(function (record) {
     console.log(record);
     res.render('books/show', {theBook: record});
+  });
+});
+
+router.get('/books/:id/update', function(req, res, next) {
+  books().where({id: req.params.id}).first().then(function (record) {
+    res.render('books/update', {theBook: record});
+  });
+});
+
+router.delete('/books/:id', function(req, res, next) {
+  books().del().where({id: req.params.id}).then(function () {
+    res.redirect('/books');
+  });
+});
+
+router.put('/books/:id/update', function(req, res, next) {
+  books().select().where({id: req.params.id}).first().update({
+    name: req.body.book_name,
+    author: req.body.book_author,
+    genre: req.body.book_genre,
+    rating: req.body.book_rating
+  }).then(function () {
+    res.redirect('/books');
   });
 });
 
